a09278e6dd1a34ff7147e858eea4d70aacd1e12f
shahanvi17112021-pixel <shahanvi17112021@gmail.com>
2025-11-03 18:26:08 +0530

    Initial upload of proposal website files.

---
+++ script.js
@@ -0,0 +1,9 @@
+function yes() {
+    document.getElementById('response').innerHTML = "<p>Yay! I can't wait to celebrate with you! 💕</p>";
+    // Add confetti or redirect to a celebration page if desired
+}
+
+function no() {
+    document.getElementById('response').innerHTML = "<p>That's okay, I'll always cherish our friendship! 😊</p>";
+    // Or make the "No" button move away playfully
+}
